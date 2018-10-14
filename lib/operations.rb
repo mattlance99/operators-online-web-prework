920b026f792d36f8bc30879f9c8304bff59b53f3
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-14 19:28:16 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+if speed < 40 || speed > 60
+  puts "Speed is either less than 40 or greater than 60"
+  true
+else 
+  puts "Speed is in between 40 and 60"
+  false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false 
 end
 	
 
